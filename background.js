de78fe8931937796961d27b666f759be2e2a28ee
frankwallace <faw987@yahoo.com>
2025-01-20 13:40:21 -0500

    add json stringify to debug code.

--- background.js
+++ background.js
@@ -72,15 +72,28 @@ function migrateConfig(oldConfig, newVersion) {
 
     return newConfig;
 }
+//
+// chrome.storage.onChanged.addListener((changes, namespace) => {
+//     for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+//         console.log(
+//             `Storage key "${key}" in namespace "${namespace}" changed.`,
+//             `Old value was "${oldValue}", new value is "${newValue}".`
+//         );
+//     }
+// });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-    for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
         console.log(
             `Storage key "${key}" in namespace "${namespace}" changed.`,
-            `Old value was "${oldValue}", new value is "${newValue}".`
+            `Old value was:`,
+            oldValue ? JSON.stringify(oldValue, null, 2) : "undefined",
+            `New value is:`,
+            newValue ? JSON.stringify(newValue, null, 2) : "undefined"
         );
     }
 });
+
 //
 // const key = 'openaikey';
 // chrome.storage.local.get([key], (data) => {
